70a35f53443e022ebcebe250d00ffce6a2f756db
kacper.glowczyk <kacper.glowczyk@microsoft.wsei.edu.pl>
2023-11-06 15:04:24 +0100

    Lab2

---
+++ Laboratorium2/Controllers/BirthController.cs
@@ -0,0 +1,30 @@
+﻿using Laboratorium2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Laboratorium2.Controllers
+{
+    public class BirthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Result([FromForm] BirthViewModel model)
+        {
+            if (!model.IsValid())
+            {
+                return View("Error");
+            }
+            return View(model);
+        }
+    }
+}
+
---
+++ Laboratorium3(App)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Laboratorium3_App_.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
